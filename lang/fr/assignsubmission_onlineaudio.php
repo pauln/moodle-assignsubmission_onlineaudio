3c3d41beb080e40b8aa0f28b133b942ba0f702e9
Guillaume Allegre <guillaume.allegre@silecs.info>
2013-02-27 11:26:51 +0100

    added french translations

---
+++ lang/fr/assignsubmission_onlineaudio.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'assignsubmission_onlineaudio', language 'fr'
+ *
+ * @package assignsubmission_onlineaudio
+ * @copyright 2012 Paul Nicholls
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+$string['configmaxbytes'] = 'Taille limite de fichier';
+$string['enabled'] = 'Enregistrement audio en ligne';
+$string['enabled_help'] = 'Ce plugin autorise la remise de devoirs sous la forme d\'enregistrements audio.';
+$string['recording'] = 'Enregistrement audio en ligne';
+$string['maxbytes'] = 'Taille limite de fichier';
+$string['maxfilessubmission'] = 'Nombre maximal d\'enregistrements';
+$string['maxfilessubmission_help'] = 'Si le plugin est activé, chaque étudiant sera autorisé à remettre au maximum ce nombre d\'enregistrements.';
+$string['pluginname'] = 'Enregistrement audio en ligne';
+$string['countfiles'] = '{$a} fichiers';
+$string['nosuchfile'] = 'Aucun fichier correspondant.';
+$string['confirmdeletefile'] = 'Êtes vous certain de vouloir supprimer le fichier <strong>{$a}</strong>?';
+$string['upload'] = 'Envoyer';
+$string['uploaderror'] = 'Erreur pendant l\'envoi de l\'enregistrement.';
+$string['maxfilesreached'] = 'Vous avez déjà atteint le nombre maximal de remises pour ce devoir.';
