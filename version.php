df1bcd4fa9e04ab1b1c5d2f557e3eacdafe06887
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2014-12-02 14:50:01 +1300

    Version bump to 1.2.1

--- version.php
+++ version.php
@@ -22,8 +22,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version   = 2014112400;
-$plugin->release   = "1.2.0";
+$plugin->version   = 2014112401;
+$plugin->release   = "1.2.1";
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2013051400;
 
